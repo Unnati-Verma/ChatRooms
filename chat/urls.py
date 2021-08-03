722225c39e39a06af5ff5821e4e09dfdb902b775
Unnati Verma <unnatiwork18@gmail.com>
2021-08-03 23:06:41 +0530

    Final Commit

--- chat/urls.py
+++ chat/urls.py
@@ -3,8 +3,8 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), #takes main page
-    path('<str:room_name>/', views.room, name='room'), #takes to the chat room
+    path('', views.index, name='index'),  # takes main page
+    path('<str:room_name>/', views.room, name='room'),  # takes to the chat room
 ]
 
 '''
